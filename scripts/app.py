9af8c576441c504a35f50339db8f60294dd9d425
Ricky Menezes Bruggemann <ricky@cedae.com.br>
2020-04-20 14:02:11 -0300

    Configuração para troca de senha no proximo logon

--- scripts/app.py
+++ scripts/app.py
@@ -20,7 +20,7 @@ s = winrm.Session(hostname, auth=(username,password),transport='ntlm')
 
 ps_script1 = """$manager = (get-aduser """+manager+""" |select distinguishedname).distinguishedname
 New-ADUser -Name \""""+nome+"""\" -SamAccountName """+usuario+""" -UserPrincipalName """+upn+""" `
--EmailAddress """+email+""" -Path "OU=INSTITUCIONAIS,OU=E-MAIL,DC=cedae,DC=corp" -AccountPassword(ConvertTo-SecureString "cedae#4455" -AsPlainText -force) `
+-EmailAddress """+email+""" -Path "OU=INSTITUCIONAIS,OU=E-MAIL,DC=cedae,DC=corp" -ChangePasswordAtLogon $true -AccountPassword(ConvertTo-SecureString "cedae#4455" -AsPlainText -force) `
 -Enabled $true -LogonWorkstations "DNS1,DNS2,DNS3" -company CEDAE -description Institucionais -Manager \"$manager\" -DisplayName \""""+nome+"""\" `
 -GivenName \""""+givenname+"""\" -Surname \""""+surname+"""\"
 Add-ADGroupMember -Identity """+grupo+""" -Members """+usuario
